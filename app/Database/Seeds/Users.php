bdea38cae4b08f56e4c4113f9e4a6f772f48c8b4
task <cathy@newmancj.com>
2021-05-19 15:34:43 +0100

    Fixing users seed

--- app/Database/Seeds/Users.php
+++ app/Database/Seeds/Users.php
@@ -8,20 +8,22 @@ class Users extends Seeder
 {
     public function run()
     {
-            $data = [
-                [                    
-                    'firstname'         => 'Rick',
-                    'lastname'          => 'Deckard',
-                    'email'             => 'rick.deckard@sf.police',
-                    'password'          => '$2y$10$E3s6fJvxmys4Gz4nWQKGJuMi7xEi4xsPsnAf5LzCzwIotRCIHtWUm', // saveTheOwls
-                    'user_created_at'   => '2021-05-10 12:01:43',
-                    'user_updated_at'   => '2021-05-10 12:01:43',
-                    'opening_balance'   => '2000',
-                    'overdraft_limit'   => '250',
-                    'balance_alert'     => '4000',]
-            ];
+        $data = [
+                 
+            'id'                =>'',              
+            'firstname'         => 'Rick',
+            'lastname'          => 'Deckard',
+            'email'             => 'rick.deckard@sf.police',
+            'password'          => '$2y$10$E3s6fJvxmys4Gz4nWQKGJuMi7xEi4xsPsnAf5LzCzwIotRCIHtWUm', // saveTheOwls
+            'user_created_at'   => '2021-05-10 12:01:43',
+            'user_updated_at'   => '2021-05-10 12:01:43',
+            'opening_balance'   => '2000.00',
+            'overdraft_limit'   => '250.00',
+            'balance_alert'     => '4000.00',
+        ];
 
-            // Using Query Builder
-            $this->db->table('users')->insert($data);
+        // Using Query Builder
+        $this->db->table('users')->insert($data);
+          
     }
 }
